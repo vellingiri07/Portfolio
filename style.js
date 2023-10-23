5a57d7cfdbb00abc8e3e0a5c66a5c910a7e13657
Vellingiri <145350490+vellingiri07@users.noreply.github.com>
2023-10-23 16:10:41 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,10 +1,10 @@
-// Function to change the header background color when the page loads
+
 function changeHeaderBackgroundColor() {
     const header = document.querySelector('header');
     header.style.backgroundColor = getRandomColor();
 }
 
-// Function to generate a random color
+
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -14,14 +14,11 @@ function getRandomColor() {
     return color;
 }
 
-// Function to add a pulsating effect to the "More About Me" link
+
 function addPulsatingEffect() {
     const moreAboutLink = document.getElementById('moreabout');
     moreAboutLink.style.animation = 'pulsate 1s infinite';
 }
-
-// Event listener for changing the header background color
 window.addEventListener('load', changeHeaderBackgroundColor);
 
-// Event listener for the "More About Me" link pulsating effect
 window.addEventListener('load', addPulsatingEffect);
